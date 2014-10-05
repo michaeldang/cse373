6d8835617ad2b9664f5a7124621a7fe2cdc8b360
Michael Dang <mwldang@uw.edu>
2014-10-04 17:17:36 -0700

    Added ImageStack class to ImageEnhancer project

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/20/60328413214c00141d54890fab1192fe
@@ -0,0 +1,11 @@
+package app;
+
+import java.util.ArrayList;
+
+public class ImageStack {
+	private ArrayList imageStack;
+	
+	public ImageStack() {
+		imageStack = new ArrayList();
+	}
+}
--- ImageEnhancerWithUndoAndRedoV2/src/app/ImageStack.java
+++ ImageEnhancerWithUndoAndRedoV2/src/app/ImageStack.java
@@ -1,11 +1,59 @@
 package app;
 
+import java.io.*;
 import java.util.ArrayList;
+import java.awt.image.BufferedImage;
 
 public class ImageStack {
-	private ArrayList imageStack;
+	private ArrayList<BufferedImage> imageStack;
+	private boolean wasPoppedLast = false;
 	
 	public ImageStack() {
-		imageStack = new ArrayList();
+		imageStack = new ArrayList<BufferedImage>();
+	}
+	
+	public ImageStack(BufferedImage givenImage) {
+		imageStack = new ArrayList<BufferedImage>();
+		push(givenImage);
+	}
+	
+	public void push(BufferedImage givenImage) {
+		imageStack.add(givenImage);
+		wasPoppedLast = false;
+	}
+	
+	public BufferedImage pop() throws Exception {
+		try {
+			BufferedImage returnImage = imageStack.remove(imageStack.size() - 1);
+			wasPoppedLast = true;
+			return returnImage;
+		} catch (Exception stackEmptyError) {
+			return null;
+		}
+	}
+	
+	public BufferedImage peek() throws Exception {
+		try {
+			return imageStack.get(imageStack.size() - 1);
+		} catch (Exception e) {
+			return null;
+		}
+	}
+	
+	public boolean isEmpty() {
+		return imageStack.isEmpty();
+	}
+	
+	public void clear() {
+		imageStack.clear();
+		wasPoppedLast = false;
+	}
+	
+	public boolean popWasLast() {
+		return wasPoppedLast;
+	}
+	
+	public int getSize() {
+		return imageStack.size();
 	}
 }
